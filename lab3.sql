94a2998492f50028ecbcce480250c444cfc8e458
daothao06-dev <daothithao2k6@gmail.com>
2026-04-10 15:03:39 +0700

    update

--- lab3.sql
+++ lab3.sql
@@ -1,24 +1,80 @@
-bài 1: lấy dữ liệu sản phẩm
+CREATE TABLE sinhvien.bangkhachhang(
+	MaKH INT PRIMARY KEY,
+	TenKH VARCHAR(255),
+	SoDienThoai VARCHAR(15),
+	Email VARCHAR(100) NOT NULL,
+	DiaChi VARCHAR(255)
+);
+INSERT INTO sinhvien.bangkhachhang (MaKh, TenKH, Email, SoDienThoai, DiaChi)
+VALUES
+('1', 'Nguyen Van An', 'an.nguyen@company.com', '0901234567', 'Ha Noi'),
+('2', 'Tran Thi Binh', 'binh.tran@company.com', '0912345678', 'Hai Phong'),
+('3', 'Le Minh Cuong', 'cuong.le@company.com', '0923456789', 'Da Nang'),
+('4', 'Pham Thu Duong', 'duong.pham@company.com', '0934567890', 'Ho Chi Minh'),
+('5', 'Hoang Van En', 'en.hoang@company.com', '0945678901', 'Can Tho'),
+('6', 'Vo Thi Phuong', 'phuong.vo@company.com', '0956789012', 'Hue'),
+('7', 'Do Minh Giang', 'giang.do@company.com', '0967890123', 'Nghe An'),
+('8', 'Bui Thi Hoa', 'hoa.bui@company.com','0967890563', 'Nam Dinh'),
+('9', 'Ly Van Ich', 'ich.ly@company.com', '0978901234', 'Quang Ninh'),
+('10', 'Mai Thi Kim', 'kim.mai@company.com', '0989012345', 'Ha Noi');
+
+
+CREATE TABLE sinhvien.bangsanpham(
+	MaSP INT PRIMARY KEY,
+	Product_name VARCHAR(255),
+	Price DECIMAL(10,2)
+);
+INSERT INTO sinhvien.bangsanpham(MaSP, Product_name, Price)
+VALUES
+('1', 'banhmi', '25000'),
+('2', 'snack','15000'),
+('3','coca','10000'),
+('4','suaconbo','27000'),
+('5','nuoccam','17000'),
+('6','nuocep','22000'),
+('7','nuocepcam','32000'),
+('8','banhmikinhdo','30000'),
+('9','traicay','67000'),
+('10','saurieng','50000');
+
+
+CREATE TABLE sinhvien.banghoadon(
+	MaHD INT PRIMARY KEY,
+	NgayHD TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	MaKH INT,
+	TongTien DECIMAL(10,2),
+	CONSTRAINT fk_bangkhachhang FOREIGN KEY (MaKH) REFERENCES sinhvien.bangkhachhang(MaKH)
+);
+CREATE TABLE sinhvien.bangchitiethoadon(
+	MaHD INT,
+	MaSP INT,
+	Soluong INT NOT NULL CHECK (Soluong >0),
+	DonGiaBan DECIMAL(10,2) NOT NULL,
+	PRIMARY KEY (MaHD, MaSP),
+	CONSTRAINT fk_banghoadon FOREIGN KEY (MaHD) REFERENCES sinhvien.banghoadon(MaHD),
+	CONSTRAINT fk_bangsanpham FOREIGN KEY (MaSP) REFERENCES sinhvien.bangsanpham(MaSP)
+);
+SELECT * FROM sinhvien.bangkhachhang;
+-- lab3 
 SELECT 
-    product_name AS TenSanPham, 
-    price AS DonGia
-FROM products;
-bài 2: tìm kiếm khách hàng theo tên
-SELECT 
-    full_name, 
-    phone_number
-FROM customers
-WHERE full_name LIKE '%Văn%';
-bài 3: sắp xếp sản phẩm theo giá
-SELECT 
-    product_name, 
-    price
-FROM products
-ORDER BY price DESC;
-bài 4: tìm 3 sản phẩm rẻ nhất
-SELECT 
-    product_name, 
-    price
-FROM products
-ORDER BY price ASC
-LIMIT 3;
+    Product_name AS TenSanPham, 
+    Price AS DonGia
+FROM 
+    sinhvien.bangsanpham;
+
+
+SELECT  MaKH, TenKH, SoDienThoai
+FROM sinhvien.bangkhachhang
+WHERE TenKh LIKE '%An%';
+
+
+SELECT Product_name, Price
+FROM sinhvien.bangsanpham
+ORDER BY Price DESC;
+
+
+
+SELECT Product_name, Price
+FROM sinhvien.bangsanpham
+ORDER BY Price
+LIMIT 3; 
--- lab4.sql
+++ lab4.sql
@@ -1,69 +1,36 @@
-bài 1: tạo toàn bộ cấu trúc CSDL bằng câu lệnh
--- 1. Tạo bảng Khách hàng
-CREATE TABLE KhachHang (
-    MaKH INT PRIMARY KEY AUTO_INCREMENT,
-    TenKH VARCHAR(255) NOT NULL,
-    SoDienThoai VARCHAR(15),
-    DiaChi VARCHAR(255)
-);
+CREATE TABLE sinhvien.suppliers(
+	Supplier_id SERIAL PRIMARY KEY ,
+	Supplier_name VARCHAR(255) NOT NULL,
+	Contact_phone VARCHAR(15) UNIQUE
+); 
 
--- 2. Tạo bảng Sản phẩm
-CREATE TABLE SanPham (
-    MaSP INT PRIMARY KEY AUTO_INCREMENT,
-    TenSP VARCHAR(255) NOT NULL,
-    DonGia DECIMAL(10, 2) NOT NULL CHECK (DonGia > 0)
-);
 
--- 3. Tạo bảng Hóa đơn
-CREATE TABLE HoaDon (
-    MaHD INT PRIMARY KEY,
-    NgayHD TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    MaKH INT,
-    TongTien DECIMAL(10, 2),
-    FOREIGN KEY (MaKH) REFERENCES KhachHang(MaKH)
-);
+ALTER TABLE sinhvien.suppliers 
+ADD COLUMN email VARCHAR(100);
 
--- 4. Tạo bảng Chi tiết hóa đơn
-CREATE TABLE ChiTietHoaDon (
-    MaHD INT,
-    MaSP INT,
-    SoLuong INT NOT NULL CHECK (SoLuong > 0),
-    PRIMARY KEY (MaHD, MaSP), -- Composite Key
-    FOREIGN KEY (MaHD) REFERENCES HoaDon(MaHD),
-    FOREIGN KEY (MaSP) REFERENCES SanPham(MaSP)
-);
-bài 2: tạo bảng nhà cung cấp
-CREATE TABLE suppliers (
-    supplier_id INT PRIMARY KEY AUTO_INCREMENT,
-    supplier_name VARCHAR(255) NOT NULL,
-    contact_phone VARCHAR(15) UNIQUE
-);
-bài 3: cập nhật cấu trúc bảng
-1, thêm cột email vào bảng suppliers
-ALTER TABLE suppliers 
-ADD email VARCHAR(100);
-2, liên kết bảng products với bảng suppliers
-bước 1 thêm cột supplier_id vào bảng products
-ALTER TABLE products 
-ADD supplier_id INT;
-bước 2 tạo ràng buộc khóa 
-ALTER TABLE products 
-ADD CONSTRAINT fk_products_suppliers 
-FOREIGN KEY (supplier_id) REFERENCES suppliers(supplier_id);
-bài 4: thao tác với dữ liệu nhà cung cấp và sản phẩm
-INSERT INTO suppliers (supplier_name, contact_phone, email)
-VALUES 
-('Công ty Sữa Việt Nam', '0987654321', 'contact@vinamilk.vn'),
+ALTER TABLE sinhvien.bangsanpham
+ADD COLUMN Supplier_id INT ;
+
+
+ALTER TABLE sinhvien.bangsanpham 
+ADD CONSTRAINT fk_sinhvien_bangsanpham_Suppliers 
+FOREIGN KEY (Supplier_id) REFERENCES sinhvien.Suppliers(Supplier_id);
+
+
+INSERT INTO sinhvien.Suppliers (Supplier_name, Contact_phone, Email) 
+VALUES ('Công ty Sữa Việt Nam', '0987654321', 'contact@vinamilk.vn'),
 ('Công ty Thực phẩm Á Châu', '0912345678', 'contact@acecook.vn');
 
-UPDATE suppliers
-SET contact_phone = '0911112222'
-WHERE supplier_name = 'Công ty Thực phẩm Á Châu';
 
-DELETE FROM products
-WHERE product_id = 8;
+SELECT * FROM sinhvien.Suppliers;
 
-bài 5: thao tác với dữ liệu nhà cung cấp và sản phẩm 
+
+UPDATE sinhvien.Suppliers 
+SET Contact_phone = '0911112222'
+WHERE Supplier_name = 'Công ty Thực phẩm Á Châu'
+
+
+
 CREATE TABLE test_table (
     id INT
 );
@@ -72,3 +39,9 @@ ALTER TABLE suppliers
 DROP COLUMN contact_phone;
 
 DROP TABLE test_table;
+
+
+
+SELECT c.customeridcustomerid, 
+from customers c
+inner join 
